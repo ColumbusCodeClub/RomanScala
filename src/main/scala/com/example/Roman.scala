01bb154cda7c1189d7b45c38929aceefb9ecc8b5
unknown <ma1t7i8@L-OFC3QP9V32.americas.gap.com>
2015-08-28 15:29:03 -0400

    Changes from 8/28

--- src/main/scala/com/example/Roman.scala
+++ src/main/scala/com/example/Roman.scala
@@ -2,10 +2,18 @@ package com.example
 
 object Roman {
  
-  
-  def convertToRoman( i : Integer ) : String ={
-      
-    (0 to (i-1)).map( x=>"I").mkString("")
+  val mapping1 = Map[Int, String](1->"I",
+                    2->"II",
+                    3->"III",
+                    4->"IV",
+                    5->"V",
+                    6->"VI",
+                    7->"VII",
+                    8->"VIII",
+                    9->"IX")
+
+  def convertToRoman( i : Int) : String = {
+     mapping1.get(i)
   }
   
 }
--- src/test/scala/RomanSuite.scala
+++ src/test/scala/RomanSuite.scala
@@ -22,4 +22,32 @@ class RomanSuite extends FunSuite  {
     assert("III"==subject.convertToRoman(3))
   }
 
+  test("Number 4 returns  IV"){
+    assert("IV"==subject.convertToRoman(4))
+  }
+
+  test("Number 5 returns V"){
+    assert("V"==subject.convertToRoman((5)))
+  }
+
+  test("Number 6 returns VI"){
+    assert("VI"==subject.convertToRoman((6)))
+  }
+  test("Number 7 returns VII"){
+    assert("VII"==subject.convertToRoman((7)))
+  }
+
+  test("Number 8 returns VIII"){
+    assert("VIII"==subject.convertToRoman((8)))
+  }
+
+  test("Number 9 returns IX"){
+    assert("IX"==subject.convertToRoman((9)))
+  }
+
+
+  //test("Number 10 returns X"){
+  //  assert("X"===subject.convertToRoman(10))
+  //}
+
 }
