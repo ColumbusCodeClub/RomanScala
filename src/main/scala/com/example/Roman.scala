227ff31117665d0c9ba61e28fca247d58483b98e
unknown <Na2j7y1@L-OFCC93MK12.americas.gap.com>
2015-08-31 12:53:19 -0400

    Adding Roman scala solution with testsuite

--- src/main/scala/com/example/Roman.scala
+++ src/main/scala/com/example/Roman.scala
@@ -1,36 +1,102 @@
 package com.example
 
+import scala.collection.MapLike
+
 object Roman {
- 
-<<<<<<< HEAD
-  val mapping1 = Map[Int, String](1->"I",
-                    2->"II",
-                    3->"III",
-                    4->"IV",
-                    5->"V",
-                    6->"VI",
-                    7->"VII",
-                    8->"VIII",
-                    9->"IX")
 
-  def convertToRoman( i : Int) : String = {
-     mapping1.get(i)
-=======
-  
-  def convertToRoman( i : Int ) : String = i match {
-    case 1|2|3 => map123(i)
-    case 4 => convertToRoman(i-3)+"V"
-    case 5 => "V"
-    case 6|7|8 => "V"+convertToRoman(i-5)
-    case 9 => "IX"
-    case 10 => "X"
-    case 11 => "XI"
-    case _ => null
+  val mapping1 = Map[Int, String](
+    1 -> "I",
+    2 -> "II",
+    3 -> "III",
+    4 -> "IV",
+    5 -> "V",
+    6 -> "VI",
+    7 -> "VII",
+    8 -> "VIII",
+    9 -> "IX"
+  )
+
+  val mapping2 = Map[Int, String](
+    1 -> "X",
+    2 -> "XX",
+    3 -> "XXX",
+    4 -> "XL",
+    5 -> "L",
+    6 -> "LX",
+    7 -> "LXX",
+    8 -> "LXXX",
+    9 -> "XC"
+  )
+
+  val mapping3 = Map[Int, String](
+    1 -> "C",
+    2 -> "CC",
+    3 -> "CCC",
+    4 -> "CD",
+    5 -> "D",
+    6 -> "DC",
+    7 -> "DCC",
+    8 -> "DCCC",
+    9 -> "CM"
+  )
+
+  val mapping4 = Map[Int, String](
+    1 -> "M",
+    2 -> "MM",
+    3 -> "MMM",
+    4 -> "MV'",
+    5 -> "V'",
+    6 -> "V'M",
+    7 -> "V'MM",
+    8 -> "V'MMM",
+    9 -> "MX'"
+  )
+
+  val mappingList = List(mapping1, mapping2, mapping3, mapping4)
+
+  def getRomanValueForPosition(position : Int, digit : Int): String = {
+    var s1 : String = "";
+
+//    if ((iList.take(position).takeRight(1).head get digit).isEmpty == false) s1 = (iList.take(position).takeRight(1).head get digit).getOrElse("")
+    if ((mappingList.slice(position-1, position).head get digit).isEmpty == false) s1 = (mappingList.slice(position-1, position).head get digit).getOrElse("")
+
+//    2 lines above replaces the match.. case block below
+//    position match {
+//      case 1 =>
+//        if ((iList.take(position).head get digit).isEmpty == false) s1 = (iList.take(position).head get digit).getOrElse("")
+////        if ((mapping1 get digit).isEmpty == false) s1 = (mapping1 get digit).getOrElse("")
+//      case 2 =>
+//        if ((mapping2 get digit).isEmpty == false) s1 = (mapping2 get digit).getOrElse("")
+//      case 3 =>
+//        if ((mapping3 get digit).isEmpty == false) s1 = (mapping3 get digit).getOrElse("")
+//      case 4 =>
+//        if ((mapping4 get digit).isEmpty == false) s1 = (mapping4 get digit).getOrElse("")
+//    }
+    s1 //returns the value
   }
 
-  def map123(x: Int): String = {
-    (0 to (x - 1)).map(y => "I").mkString("")
->>>>>>> 6351f26ac66cc1474ccb14e910446e3d1a100f5c
+  def convertToRoman(originalNumber: Int): String = {
+    var currentPosition: Int = 1
+    var finalRomanVal: String = ""
+    var number : Int = originalNumber
+
+    while (number > 0) {
+      var currentDigit: Int = number % 10
+      var newDeci : Int = number / 10
+
+      if (currentDigit != 0) {
+        var romanStr: String = getRomanValueForPosition(currentPosition, currentDigit)
+
+        if (romanStr.isEmpty == false) {
+          romanStr += finalRomanVal
+          finalRomanVal = romanStr
+        }
+      }
+      currentPosition += 1
+      number = newDeci
+    }
+    println("Number=" + originalNumber + ", Roman value:" + finalRomanVal)
+    finalRomanVal
   }
+
 }
- 
--- src/test/scala/RomanSuite.scala
+++ src/test/scala/RomanSuite.scala
@@ -9,6 +9,9 @@ class RomanSuite extends FunSuite  {
  val subject = Roman
 
 
+  test("Number 0 returns  nothing"){
+    assert(""==subject.convertToRoman(0))
+  }
 
   test("Number 1 returns  I"){
     assert("I"==subject.convertToRoman(1))
@@ -26,7 +29,6 @@ class RomanSuite extends FunSuite  {
     assert("IV"==subject.convertToRoman(4))
   }
 
-<<<<<<< HEAD
   test("Number 5 returns V"){
     assert("V"==subject.convertToRoman((5)))
   }
@@ -46,19 +48,72 @@ class RomanSuite extends FunSuite  {
     assert("IX"==subject.convertToRoman((9)))
   }
 
-
-  //test("Number 10 returns X"){
-  //  assert("X"===subject.convertToRoman(10))
-  //}
-
-=======
-  test("Number 5 returns  V"){
-    assert("V"==subject.convertToRoman(5))
+  test("Number 10 returns X"){
+    assert("X"==subject.convertToRoman((10)))
   }
-  
-  test( "Number 6 returns VI")
-  {
-    assert("VI" == subject.convertToRoman(6))
+
+  test("Number 25 returns XXV"){
+    assert("XXV"==subject.convertToRoman((25)))
   }
->>>>>>> 6351f26ac66cc1474ccb14e910446e3d1a100f5c
+
+  test("Number 50 returns L"){
+    assert("L"==subject.convertToRoman((50)))
+  }
+
+  test("Number 99 returns XCIX"){
+    assert("XCIX"==subject.convertToRoman((99)))
+  }
+
+  test("Number 100 returns C"){
+    assert("C"==subject.convertToRoman((100)))
+  }
+
+  test("Number 101 returns CI"){
+    assert("CI"==subject.convertToRoman((101)))
+  }
+
+  test("Number 125 returns CXXV"){
+    assert("CXXV"==subject.convertToRoman((125)))
+  }
+
+  test("Number 500 returns D"){
+    assert("D"==subject.convertToRoman((500)))
+  }
+
+  test("Number 888 returns DCCCLXXXVIII"){
+    assert("DCCCLXXXVIII"==subject.convertToRoman((888)))
+  }
+
+  test("Number 1000 returns M"){
+    assert("M"==subject.convertToRoman((1000)))
+  }
+
+  test("Number 1888 returns MDCCCLXXXVIII"){
+    assert("MDCCCLXXXVIII"==subject.convertToRoman((1888)))
+  }
+
+  test("Number 1986 returns MCMLXXXVI"){
+    assert("MCMLXXXVI"==subject.convertToRoman((1986)))
+  }
+
+  test("Number 2015 returns MMXV"){
+    assert("MMXV"==subject.convertToRoman((2015)))
+  }
+
+  test("Number 3888 returns MMMDCCCLXXXVIII"){
+    assert("MMMDCCCLXXXVIII"==subject.convertToRoman((3888)))
+  }
+
+  test("Number 5000 returns " + "V'"){
+    assert("V'"==subject.convertToRoman((5000)))
+  }
+
+  test("Number 6000 returns V'M"){
+    assert("V'M"==subject.convertToRoman((6000)))
+  }
+
+  test("Number 8888 returns V'MMMDCCCLXXXVIII"){
+    assert("V'MMMDCCCLXXXVIII"==subject.convertToRoman((8888)))
+  }
+
 }
